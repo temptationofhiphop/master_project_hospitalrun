5d4317a77cb3ad4fb4034193da61f7ec572e6875
Alisher <alisher.nosirov@equinox-fintech.de>
2026-06-29 20:50:44 +0200

    Added the first experiment changes

--- src/medications/ViewMedication.tsx
+++ src/medications/ViewMedication.tsx
@@ -1,122 +1,29 @@
 import { Select, Row, Column, Badge, Button, Alert, Label } from '@hospitalrun/components'
 import format from 'date-fns/format'
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams, useHistory } from 'react-router-dom'
+import React from 'react'
 
-import useAddBreadcrumbs from '../page-header/breadcrumbs/useAddBreadcrumbs'
-import { useUpdateTitle } from '../page-header/title/TitleContext'
-import { SelectOption } from '../shared/components/input/SelectOption'
 import TextFieldWithLabelFormGroup from '../shared/components/input/TextFieldWithLabelFormGroup'
 import TextInputWithLabelFormGroup from '../shared/components/input/TextInputWithLabelFormGroup'
-import useTranslator from '../shared/hooks/useTranslator'
-import Medication from '../shared/model/Medication'
-import Patient from '../shared/model/Patient'
-import Permissions from '../shared/model/Permissions'
-import { RootState } from '../shared/store'
-import { cancelMedication, updateMedication, fetchMedication } from './medication-slice'
-
-const getTitle = (patient: Patient | undefined, medication: Medication | undefined) =>
-  patient && medication ? `${medication.medication} for ${patient.fullName}` : ''
+import useViewMedication from './hooks/useViewMedication'
 
 const ViewMedication = () => {
-  const { id } = useParams<{ id: string }>()
-  const { t } = useTranslator()
-  const history = useHistory()
-  const dispatch = useDispatch()
-  const { permissions } = useSelector((state: RootState) => state.user)
-  const { medication, patient, status, error } = useSelector((state: RootState) => state.medication)
-
-  const [medicationToView, setMedicationToView] = useState<Medication>()
-  const [isEditable, setIsEditable] = useState<boolean>(true)
-
-  const updateTitle = useUpdateTitle()
-  useEffect(() => {
-    updateTitle(getTitle(patient, medicationToView))
-  })
-
-  const breadcrumbs = [
-    {
-      i18nKey: 'medications.requests.view',
-      location: `/medications/${medicationToView?.id}`,
-    },
-  ]
-  useAddBreadcrumbs(breadcrumbs)
-
-  useEffect(() => {
-    if (id) {
-      dispatch(fetchMedication(id))
-    }
-  }, [id, dispatch])
-
-  useEffect(() => {
-    if (medication) {
-      setMedicationToView({ ...medication })
-      setIsEditable(medication.status !== 'completed')
-    }
-  }, [medication])
-
-  const statusOptionsEdit: SelectOption[] = [
-    { label: t('medications.status.draft'), value: 'draft' },
-    { label: t('medications.status.active'), value: 'active' },
-    { label: t('medications.status.onHold'), value: 'on hold' },
-    { label: t('medications.status.completed'), value: 'completed' },
-    { label: t('medications.status.enteredInError'), value: 'entered in error' },
-    { label: t('medications.status.canceled'), value: 'canceled' },
-    { label: t('medications.status.unknown'), value: 'unknown' },
-  ]
-
-  const intentOptions: SelectOption[] = [
-    { label: t('medications.intent.proposal'), value: 'proposal' },
-    { label: t('medications.intent.plan'), value: 'plan' },
-    { label: t('medications.intent.order'), value: 'order' },
-    { label: t('medications.intent.originalOrder'), value: 'original order' },
-    { label: t('medications.intent.reflexOrder'), value: 'reflex order' },
-    { label: t('medications.intent.fillerOrder'), value: 'filler order' },
-    { label: t('medications.intent.instanceOrder'), value: 'instance order' },
-    { label: t('medications.intent.option'), value: 'option' },
-  ]
-
-  const priorityOptions: SelectOption[] = [
-    { label: t('medications.priority.routine'), value: 'routine' },
-    { label: t('medications.priority.urgent'), value: 'urgent' },
-    { label: t('medications.priority.asap'), value: 'asap' },
-    { label: t('medications.priority.stat'), value: 'stat' },
-  ]
-
-  const onQuantityChange = (text: string, name: string) => {
-    const newMedication = medicationToView as Medication
-    setMedicationToView({ ...newMedication, quantity: { ...newMedication.quantity, [name]: text } })
-  }
-
-  const onFieldChange = (key: string, value: string | boolean) => {
-    const newMedication = medicationToView as Medication
-    setMedicationToView({ ...newMedication, [key]: value })
-  }
-
-  const onNotesChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const notes = event.currentTarget.value
-    onFieldChange('notes', notes)
-  }
-
-  const onUpdate = async () => {
-    const onSuccess = () => {
-      history.push('/medications')
-    }
-    if (medicationToView) {
-      dispatch(updateMedication(medicationToView, onSuccess))
-    }
-  }
-
-  const onCancel = async () => {
-    const onSuccess = () => {
-      history.push('/medications')
-    }
-
-    if (medicationToView) {
-      dispatch(cancelMedication(medicationToView, onSuccess))
-    }
-  }
+  const {
+    canCancelMedication,
+    error,
+    intentOptions,
+    isEditable,
+    medicationToView,
+    onCancel,
+    onFieldChange,
+    onNotesChange,
+    onQuantityChange,
+    onUpdate,
+    patient,
+    priorityOptions,
+    status,
+    statusOptionsEdit,
+    t,
+  } = useViewMedication()
 
   const getButtons = () => {
     const buttons: React.ReactNode[] = []
@@ -130,7 +37,7 @@ const ViewMedication = () => {
       </Button>,
     )
 
-    if (permissions.includes(Permissions.CancelMedication)) {
+    if (canCancelMedication) {
       buttons.push(
         <Button onClick={onCancel} color="danger" key="medications.requests.cancel">
           {t('medications.requests.cancel')}
